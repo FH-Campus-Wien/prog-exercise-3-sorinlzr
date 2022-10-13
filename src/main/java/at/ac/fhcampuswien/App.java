b8230933474ae71d31689623d5a3658f3c335af6
sorin.lzr <soso.laz@gmail.com>
2022-10-13 23:08:40 +0200

    [prog-exercise-3] implement all tasks

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,9 +1,15 @@
 package at.ac.fhcampuswien;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+
+    private static final int UPPERCASE_CHAR_MIN_VALUE = 65;
+    private static final int UPPERCASE_CHAR_MAX_VALUE = 90;
+    private static final int LOWERCASE_CHAR_MIN_VALUE = 97;
+    private static final int LOWERCASE_CHAR_MAX_VALUE = 122;
+    private static final int WHITESPACE_CHAR_MIN_VALUE = 28;
+    private static final int WHITESPACE_CHAR_MAX_VALUE = 32;
 
     public static void oneMonthCalendar(int daysInMonth, int firstDay) {
         int offsetDays = firstDay - 1;
@@ -50,12 +56,119 @@ public class App {
         return (factor * seed + increment) % modul;
     }
 
+    public static void guessingGame(int numberToGuess) {
+        Scanner scan = new Scanner(System.in);
+        int counter = 1;
+        int guess;
+
+        while (true) {
+            System.out.printf("Guess number %d: ", counter);
+            guess = scan.nextInt();
+
+            if (guess == numberToGuess) {
+                System.out.println("You won wisenheimer!");
+                break;
+            } else if (counter == 10) {
+                System.out.println("You lost! Have you ever heard of divide & conquer?");
+                break;
+            } else if (guess > numberToGuess) {
+                System.out.println("The number AI picked is lower than your guess.");
+            } else {
+                System.out.println("The number AI picked is higher than your guess.");
+            }
+            counter++;
+        }
+    }
+
+    public static int randomNumberBetweenOneAndHundred() {
+        return 1 + (int) (Math.random()*100);
+    }
+
+    public static boolean swapArrays(int[] firstArray, int[] secondArray) {
+        if (firstArray.length != secondArray.length) return false;
+        int[] tempArray = new int[firstArray.length];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            tempArray[i] = firstArray[i];
+            firstArray[i] = secondArray[i];
+            secondArray[i] = tempArray[i];
+        }
+        return true;
+    }
+
+    private static void printArrays(int[] firstArr, int[] secondArr) {
+        System.out.print("First array: ");
+        for (int i : firstArr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.print("Second array: ");
+        for (int i : secondArr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static String camelCase(String sentence) {
+        char[] sentenceCharacters = sentence.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        boolean isNextUppercase = true;
+
+        for (char c : sentenceCharacters) {
+            if(!isValidLetter(c) && !isWhitespace(c)){
+                continue;
+            }
+            if (isWhitespace(c)) {
+                isNextUppercase = true;
+                continue;
+            }
+            c = Character.toLowerCase(c);
+
+            if (isNextUppercase) {
+                sb.append(Character.toUpperCase(c));
+                isNextUppercase = false;
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static boolean isValidLetter(char c) {
+        return UPPERCASE_CHAR_MIN_VALUE <= c && c <= UPPERCASE_CHAR_MAX_VALUE
+                || LOWERCASE_CHAR_MIN_VALUE <= c && c <= LOWERCASE_CHAR_MAX_VALUE;
+    }
+
+    private static boolean isWhitespace(char c) {
+        return WHITESPACE_CHAR_MIN_VALUE <= c && c <= WHITESPACE_CHAR_MAX_VALUE;
+    }
+
+    public static int checkDigit(int[] code) {
+        int cypher;
+        int sum = 0;
+
+        for (int i = 0; i < code.length; i ++) {
+            int weight = i + 2;
+            int product = weight * code[i];
+            sum += product;
+        }
+
+        int dif = 11 - (sum % 11);
+
+        if (dif == 10) cypher = 0;
+        else if (dif == 11) cypher = 5;
+        else cypher = dif;
+
+        return cypher;
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Assignment 1:");
         oneMonthCalendar(31, 3);
-        System.out.println("--------------------------------");
+        System.out.println("----------------------------------------------------------------");
 
         System.out.println("Assignment 2:");
         long[] randoms = lcg(11);
@@ -63,7 +176,33 @@ public class App {
             System.out.printf("Number %d: %d", i,randoms[(int) i]);
             System.out.println();
         }
-        System.out.println("--------------------------------");
+        System.out.println("----------------------------------------------------------------");
+
+        guessingGame(randomNumberBetweenOneAndHundred());
+        System.out.println("Assignment 3:");
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("Assignment 4:");
+        int[] firstArr = {1,2,3,4,5};
+        int[] secondArr = {100,99,60,44,2};
+
+        System.out.println("Before swap:");
+        printArrays(firstArr, secondArr);
+
+        swapArrays(firstArr, secondArr);
+
+        System.out.println("After swap:");
+        printArrays(firstArr, secondArr);
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("Assignment 5:");
+        String sentence = "   nO,  rAdLeR   iS nOt a ReAl bEer!!!111";
+        System.out.println(camelCase(sentence));
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("Assignment 6:");
+        int cypher = checkDigit(new int[]{3, 9, 1, 5, 8});
+        System.out.println("Cypher is: " + cypher);
 
     }
 }
